59a490a3768c3b8d9cdbb390d689a3ec877bd494
DenisArger <den.arger@gmail.com>
2023-03-08 12:23:43 +0300

    finnaly

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Переделать любое дз с JAVA на любой ЯП (C#\python) с использованием ООП
 // Приложить исходник на Java и другом языке
-// See https://github.com/WaleriiChirkov-16021990/javaHomeworkSem4/blob/main/src/calculatorVersion3.java for more information
+// See https://github.com/DenisArger/gbJavaOopTask4 for more information
 
 Application template = new Application();
 template.run_app();
